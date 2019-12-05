37d64ed28441bd682948d59d86e27fb78565e630
asavchenkov <asavchenkov1@gmail.com>
2019-12-04 22:42:11 -0800

    added forgotten collider to door

---
+++ Assets/door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public Animator animator;
+    public BoxCollider2D coll;
+    public bool condition = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("open condition", condition);
+        coll.enabled = !condition;
+    }
+}
