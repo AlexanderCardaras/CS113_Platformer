a127db6edd7c0ba9d695d4fc0e795b91e4f56d36
asavchenkov <asavchenkov1@gmail.com>
2019-12-05 22:34:46 -0800

    started with scene 2

--- Assets/Scripts/pressure_plate.cs
+++ Assets/Scripts/pressure_plate.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class pressure_plate : MonoBehaviour
 {
     public int valid_collision_count = 0;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +15,14 @@ public class pressure_plate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "PressureValid")
         {
+            Debug.Log("entered");
             valid_collision_count++;
         }
 
@@ -32,6 +32,7 @@ public class pressure_plate : MonoBehaviour
     {
         if (collision.gameObject.tag == "PressureValid")
         {
+            Debug.Log("exited");
             valid_collision_count--;
         }
     }
